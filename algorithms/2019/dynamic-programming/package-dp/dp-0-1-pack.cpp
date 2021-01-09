36a1d37773cbd3c0256b5648a5d6777b71077dae
Jason Cheung <jasoncheung_0@foxmail.com>
2021-01-09 23:17:05 +0800

    20210109a

--- algorithms/2019/dynamic-programming/package-dp/dp-0-1-pack.cpp
+++ algorithms/2019/dynamic-programming/package-dp/dp-0-1-pack.cpp
@@ -2,7 +2,7 @@
  * 动态规划 求解0/1背包问题 改进版
  * 采用一维数组实现，空间复杂度由原本的O(VN) 降至O(V)
  * 抽象出函数dpZeroOnePack(int*,int,int) 使程序更加结构化 可读可移植
- * 无法回推得到最优解
+ * 无法回推得到最优解集合
  */
 
 #include <iostream>
@@ -19,8 +19,7 @@ void setArray(int *, int, int);
 
 int dpZeroOnePack(int *, int, int);
 
-int main()
-{
+int main() {
     int maxValue = 0;
     std::cin >> count >> maxWeight;
     int *weights = new int[count + 1];
@@ -32,9 +31,8 @@ int main()
     readArray(values + 1, count);
     setArray(dpFunction + 1, 0, maxWeight);
 
-    for (int i = 1; i <= count; i++)
-    {
-        maxValue = dpZeroOnePack(dpFunction,weights[i],values[i]);
+    for (int i = 1; i <= count; i++) {
+        maxValue = dpZeroOnePack(dpFunction, weights[i], values[i]);
     }
 
     std::cout << maxValue << std::endl;
@@ -46,36 +44,28 @@ int main()
     return 0;
 }
 
-int dpZeroOnePack(int *dpFunc, int weight, int value)
-{
-    for (int w = maxWeight; w >= weight; w--)
-    {
+int dpZeroOnePack(int *dpFunc, int weight, int value) {
+    for (int w = maxWeight; w >= weight; w--) {
         dpFunc[w] = std::max(dpFunc[w], dpFunc[w - weight] + value);
     }
     return dpFunc[maxWeight];
 }
 
-void readArray(int *array, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
+void readArray(int *array, int size) {
+    for (int i = 0; i < size; i++) {
         std::cin >> array[i];
     }
 }
 
-void printArray(int *array, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
+void printArray(int *array, int size) {
+    for (int i = 0; i < size; i++) {
         std::cout << array[i] << " ";
     }
     std::cout << std::endl;
 }
 
-void setArray(int *array, int value, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
+void setArray(int *array, int value, int size) {
+    for (int i = 0; i < size; i++) {
         array[i] = value;
     }
 }
